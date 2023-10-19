078a154a808f547f48ac75a395604165a86492be
Aliaksandr <132036102+AliaksandrKykharenka@users.noreply.github.com>
2023-10-19 19:21:09 +0300

    Update CalculatorTest.java
    Remove empty strint in CalculatorTest.java

--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -1,7 +1,6 @@
 package com.startjava.lesson_2_3_4.calculator;
 
 public class CalculatorTest {
-
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
         calculator.startCalculation();
