f7229735bb886fb099f5bff04f66a420ba47c06d
Aliaksandr <svobodafeniksby@mail.ru>
2023-10-26 14:10:31 +0300

    Add method print() in CalculatorTest

--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -6,24 +6,29 @@ public class CalculatorTest {
 
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
+        CalculatorTest calculatorTest = new CalculatorTest();
         Scanner scanner = new Scanner(System.in);
         String answer = "yes";
         do {
             if ("yes".equals(answer)) {
                 System.out.print("Enter separated by a space: first number, mathematical symbol, second number: ");
                 String mathExpression = scanner.nextLine();
-
                 double solution = calculator.calculate(mathExpression);
-                if ((solution % 1 == 0) && (solution != Double.MIN_VALUE)) {
-                    System.out.printf(mathExpression + " = %.0f \n", solution);
-                } else if ((solution % 1 != 0) && (solution != Double.MIN_VALUE)) {
-                    System.out.printf(mathExpression + " = %.3f \n", solution);
+                if (solution != Double.MIN_VALUE) {
+                    calculatorTest.print(solution, mathExpression);
                 }
-
             }
             System.out.print("Would you like to continue? Type in [yes/no]: ");
             answer = scanner.nextLine();
         }
         while (!"no".equals(answer));
+    }
+
+    private void print(double solution, String mathExpression) {
+        if (solution % 1 == 0) {
+            System.out.printf(mathExpression + " = %.0f \n", solution);
+        } else {
+            System.out.printf(mathExpression + " = %.3f \n", solution);
+        }
     }
 }
